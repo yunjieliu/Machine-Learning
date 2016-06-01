981134724e06bcd33a9d5fc0e66fbf0efd92e1e1
yunjie <yunjieliu@lbl.gov>
2016-05-31 18:08:03 -0700

    negative accuracy for minimization

--- monkey/HYPER/front/svm2/supportvectormachine.py
+++ monkey/HYPER/front/svm2/supportvectormachine.py
@@ -36,13 +36,13 @@ def hyper_opt(Cc,ga):
     score=numpy.mean(scores)
     print ("cross validation average accuracy:  %f%%" %(score*100))
 
-    return score
+    return (-1.0)*score  #minimize
 
 
 def main(job_id,params):
     print "Anything printed here will end up in the output directory for job #%d" %job_id
     print params
-    accuracy=hyper_opt(params['C'],params['G'])
+    accuracy=hyper_opt(float(params['C']),float(params['G']))
 
     return accuracy
 
