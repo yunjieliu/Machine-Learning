a9b401314575ff26272130853e833712efccf0ae
yunjie <yunjieliu@lbl.gov>
2016-08-04 16:09:47 -0700

    add weight decay regularization, not seem to work yet

--- turkey/TC_CNN_lasagne.py
+++ turkey/TC_CNN_lasagne.py
@@ -8,31 +8,33 @@ import numpy
 import data_load
 
 
-
-def CNN(input_var=None):
+def CNN(input_var):
     """
     Build ConvNet on lasagne tool box, layer structure similar to the one build on Theano for unified network
     1) use 3D convolution as the first convolution layer to deal with different channel inputs
     2) use Spatial Pyramid Pooling layer before fully connected layer to deal with different dimension input 
     """
+    In=lasagne.layers.InputLayer(shape=(None,8,32,32),input_var=input_var,name='inputlayer')
 
-    convnet=lasagne.layers.InputLayer(shape=(None,8,32,32),input_var=input_var,name='inputlayer')
-
-    convnet=lasagne.layers.Conv2DLayer(convnet,num_filters=16, filter_size=(5, 5), stride=(1, 1), pad=0, \
+    Conv1=lasagne.layers.Conv2DLayer(In,num_filters=16, filter_size=(5, 5), stride=(1, 1), pad=0, \
             nonlinearity=lasagne.nonlinearities.rectify,W=lasagne.init.GlorotUniform(),name='conv1')
 
-    convnet=lasagne.layers.MaxPool2DLayer(convnet,pool_size=(2, 2),stride=None, pad=(0, 0), ignore_border=True,name='pool1')
+    Pool1=lasagne.layers.MaxPool2DLayer(Conv1,pool_size=(2, 2),stride=None, pad=(0, 0), ignore_border=True,name='pool1')
 
-    convnet=lasagne.layers.Conv2DLayer(convnet,num_filters=32, filter_size=(5, 5), stride=(1, 1), pad=0, \
+    Lrn1=lasagne.layers.LocalResponseNormalization2DLayer(Pool1,alpha=0.001,k=1,beta=0.75,n=5)
+
+    Conv2=lasagne.layers.Conv2DLayer(Lrn1,num_filters=32, filter_size=(5, 5), stride=(1, 1), pad=0, \
             nonlinearity=lasagne.nonlinearities.rectify,W=lasagne.init.GlorotUniform(),name='conv2')
 
-    convnet=lasagne.layers.MaxPool2DLayer(convnet,pool_size=(2, 2),name='pool2')
+    Pool2=lasagne.layers.MaxPool2DLayer(Conv2,pool_size=(2, 2),name='pool2')
+   
+    Lrn2=lasagne.layers.LocalResponseNormalization2DLayer(Pool2,alpha=0.001,k=1,beta=0.75,n=5)
 
-    convnet=lasagne.layers.DenseLayer(convnet,num_units=500,nonlinearity=lasagne.nonlinearities.rectify,name='full1')
+    Full1=lasagne.layers.DenseLayer(Lrn2,num_units=500,nonlinearity=lasagne.nonlinearities.rectify,name='full1')
 
-    convnet=lasagne.layers.DenseLayer(convnet,num_units=2,nonlinearity=lasagne.nonlinearities.sigmoid,name='full2')
+    Full2=lasagne.layers.DenseLayer(Full1,num_units=2,nonlinearity=lasagne.nonlinearities.sigmoid,name='full2')
 
-    return convnet
+    return Full2 #each layer kined to its inputs, so just need output layer to get information from all layers
 
 
 #get training data
@@ -43,7 +45,7 @@ file_name="hurricanes.h5"
 data_dict=["1","0"] #group name of positive and negative examples
 train_num_p=train_num_n=8000  #positive and negative training example
 valid_num_p=valid_num_n=1000
-test_num_p=test_num_n=1000    #positive and negative testing example
+test_num_p=test_num_n=1000   #positive and negative testing example
 
 norm_type= 3    # #1: global contrast norm, 2:standard norm, 3:l1/l2 norm, scikit learn
 
@@ -56,33 +58,37 @@ norm_type= 3    # #1: global contrast norm, 2:standard norm, 3:l1/l2 norm, sciki
                                  train_num_n,valid_num_n,test_num_n,
                                  norm_type,normalize=True)
 label_train=numpy.argmax(Y_train,axis=1)
+label_valid=numpy.argmax(Y_valid,axis=1)
 #theano variables
 input_var = tensor.tensor4('inputs')
 target_var = tensor.matrix('targets')
+size=tensor.vector('size')
 
 #build CNN
 convnet=CNN(input_var)
-
-print "Great, CNN build...."
 
 #loss function, paramaters, updates (learning rules)
 prediction = lasagne.layers.get_output(convnet)
 label_predict=tensor.argmax(prediction,axis=1)
 
+#add weight decay regularization loss to total loss, total loss=data loss +regularization loss
+#Essentially this is the same as the way weight decay implemented in neon. If you take grad(Loss function)
+#will results in the form implementes in neon, where directly apply weight decay on weights, rather than through 
+#loss function
 loss= lasagne.objectives.binary_crossentropy(prediction, target_var)
+layers = {Conv1: 0.005, Conv2: 0.005, Full1: 0.005,  convnet:0.005}
+decay_reg=lasagne.regularization.regularize_layer_params(layers,lasagne.regularization.l1)
+loss=loss+decay_reg
 loss=loss.mean() 
 
 params=lasagne.layers.get_all_params(convnet,trainable=True)
 updates=lasagne.updates.momentum(loss,params,learning_rate=0.01,momentum=0.9)
 
-print "Great,gradient, loss, updates build..."
 #train function
 train=theano.function([input_var,target_var],loss,updates=updates)
 predict=theano.function([input_var],label_predict)
 
-print "Great, function build...."
 #training the model
-print "starts model training...."
 
 num_epochs=1000
 epoch=0
@@ -95,6 +101,7 @@ while epoch<num_epochs:
       for batch in range(0,len(X_train),batch_size):
           X_batch=X_train[batch:batch+batch_size]
           Y_batch=Y_train[batch:batch+batch_size]
+          bshape=X_batch.shape
           train_loss += float(train(X_batch,Y_batch))
           train_batch += 1
 
@@ -104,8 +111,8 @@ while epoch<num_epochs:
       if epoch %5 ==0: #then we print out training accuracy
          label_predict=predict(X_train)
          accuracy=numpy.mean(label_predict==label_train)
-         print "Validating accuracy %0.8f " %accuracy
+         print "Training accuracy %0.8f " %accuracy
 
- 
-    
-
+         label_predict=predict(X_valid)
+         accuracy=numpy.mean(label_predict==label_valid)
+         print "Validating accuracy %0.8f " %accuracy       
