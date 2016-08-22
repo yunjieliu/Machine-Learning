e55d43a9ef9b0321aabd9866682d2eb4c549d4ff
yunjie <yunjieliu@lbl.gov>
2016-08-22 13:05:01 -0700

    parse large dataset into small piece while evaluation

--- turkey/TC_CNN_lasagne.py
+++ turkey/TC_CNN_lasagne.py
@@ -52,7 +52,9 @@ def CNN(input_var):
    
     Lrn2=lasagne.layers.LocalResponseNormalization2DLayer(Pool2,alpha=0.001,k=1,beta=0.75,n=5)
 
-    Full1=lasagne.layers.DenseLayer(Lrn2,num_units=500,nonlinearity=lasagne.nonlinearities.rectify,name='full1')
+    drop1=lasagne.layers.DropoutLayer(Lrn2,p=0.3)
+
+    Full1=lasagne.layers.DenseLayer(drop1,num_units=1024,nonlinearity=lasagne.nonlinearities.rectify,name='full1')
 
     Full2=lasagne.layers.DenseLayer(Full1,num_units=2,nonlinearity=lasagne.nonlinearities.sigmoid,name='full2')
 
@@ -65,11 +67,11 @@ numpy.random.seed(rng_seed)
 data_dir="/global/project/projectdirs/nervana/yunjie/climate_neon1.0run/conv/DATA/"
 file_name="hurricanes.h5"
 data_dict=["1","0"] #group name of positive and negative examples
-train_num_p=train_num_n=8000  #positive and negative training example
-valid_num_p=valid_num_n=1000
-test_num_p=test_num_n=1000   #positive and negative testing example
+train_num_p=train_num_n=10  #positive and negative training example
+valid_num_p=valid_num_n=5000
+test_num_p=test_num_n=10  #positive and negative testing example
 
-norm_type= 3    # #1: global contrast norm, 2:standard norm, 3:l1/l2 norm, scikit learn
+norm_type= 2    # #1: global contrast norm, 2:standard norm, 3:l1/l2 norm, scikit learn
 
 #get training , testing data
 #hurricane image size 8x32x32
@@ -110,7 +112,7 @@ loss=loss.mean()
 
 params=lasagne.layers.get_all_params(convnet,trainable=True)
 #updates=lasagne.updates.momentum(loss,params,learning_rate=0.01,momentum=0.9)
-updates=momentum_decay(loss,params,learning_rate=0.01,momentum=0.9,decay=0.01)
+updates=momentum_decay(loss,params,learning_rate=0.02,momentum=0.9,decay=0.01)
 
 #train function
 train=theano.function([input_var,target_var],loss,updates=updates)
@@ -120,7 +122,7 @@ predict=theano.function([input_var],label_predict)
 
 num_epochs=1000
 epoch=0
-batch_size=128
+batch_size=10
 train_loss=0
 train_batch=0
 
@@ -137,17 +139,31 @@ while epoch<num_epochs:
       epoch=epoch+1
 
       if epoch %5 ==0: #then we print out training accuracy
-         label_predict=predict(X_train)
+         label_predict=[]
+         for batch in range(0,len(X_train),batch_size):
+             X_batch=X_train[batch:batch+batch_size]
+             label_predict.extend(predict(X_batch))
          accuracy=numpy.mean(label_predict==label_train)
-         print "Training accuracy %0.8f " %accuracy
+         print("Training accuracy %0.8f " %accuracy)
 
-         label_predict=predict(X_valid)
+         label_predict=[] #valid accuracy
+         for batch in range(0,len(X_valid),batch_size):
+             X_batch=X_valid[batch:batch+batch_size]
+             label_predict.extend(predict(X_batch))
          accuracy=numpy.mean(label_predict==label_valid)
-         print "Validating accuracy %0.8f " %accuracy   
+         print ("Validating accuracy %0.8f " %accuracy)
+
+         #label_predict=predict(X_train)
+         #accuracy=numpy.mean(label_predict==label_train)
+         #print "Training accuracy %0.8f " %accuracy
+
+         #label_predict=predict(X_valid)
+         #accuracy=numpy.mean(label_predict==label_valid)
+         #print "Validating accuracy %0.8f " %accuracy   
 
 
 #when model trained well, save the trained weights
-numpy.savez('TC_CNN.npz', *lasagne.layers.get_all_param_values(convnet)) 
+#numpy.savez('TC_CNN.npz', *lasagne.layers.get_all_param_values(convnet)) 
 
 #to load trained weights to model
 #with np.load('TC_CNN.npz') as f:
